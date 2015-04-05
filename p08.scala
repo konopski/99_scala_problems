104a5eccdd166b8bd82ed1fc75b56dc878de2b20
Lukasz Konopski <konopski@gmail.com>
2015-04-05 21:51:26 +0200

    problems 8 and 9

---
+++ p08.scala
@@ -0,0 +1,16 @@
+def compress[A](list: List[A]) = {
+  def compr1(list: List[A], prev: A, acc: List[A]): List[A]= {
+    if(list.isEmpty) acc 
+    else {
+      if(list.head == prev) compr1(list.tail, prev, acc)
+      else compr1(list.tail, list.head, acc :+ list.head)
+    }
+  }
+  
+  if(list.isEmpty) list
+  else compr1(list.tail, list.head, List(list.head))
+}
+
+
+println( compress(List('a)) )
+println( compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) )
---
+++ p09.scala
@@ -0,0 +1,10 @@
+def pack[A](list: List[A]) = {
+ 
+  List()
+}
+
+
+val result = pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+println(result)
+println(result == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+
