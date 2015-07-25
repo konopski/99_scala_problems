be925ea6ed97a757c184ff70a7d006b90927cbba
Lukasz Konopski <konopski@gmail.com>
2015-07-25 08:45:04 +0200

    p09 pack (without previous solutions)

---
+++ pack.scala
@@ -0,0 +1,28 @@
+object P09 extends App{
+
+ def pack[T](list: List[T]): List[List[T]] = {
+
+  def last[T] (l: List[T]) = l.reverse.head
+
+  def replaceLast[T] (l: List[List[T]], elem: List[T]) = (elem :: l.reverse.tail).reverse
+
+  def p0[T](acc: List[List[T]], l: List[T]) : List[List[T]] = {
+    if(l.isEmpty) acc
+    else {
+        val previousElems = last(acc)        
+        if(previousElems.head == l.head) {
+            p0(replaceLast(acc, previousElems ++ List(l.head) ), l.tail)
+        } else {
+            p0(acc ++ List(List(l.head)), l.tail)
+        }      
+    }
+  }  
+
+  if(list.isEmpty) List()
+  else p0(List(List(list.head)), list.tail) 
+
+ }
+
+ println(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+
+}
