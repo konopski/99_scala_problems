56a0184224962c0fcdc64ce727ad15687100844d
Lukasz Konopski <konopski@gmail.com>
2015-07-25 09:09:35 +0200

    run len encoding pack based

---
+++ encode.scala
@@ -0,0 +1,11 @@
+object P10 extends App {
+import P09._
+
+  def encode[T](list: List[T]) = {
+
+    pack(list) map { l => (l.size, l.head) }
+  } 
+
+ println(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+
+}
