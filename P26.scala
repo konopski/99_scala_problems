af7917792e9ead8c46835e5bcab45e34093a8eb1
Lukasz Konopski <konopski@gmail.com>
2016-02-03 07:30:10 +0100

    combinations

---
+++ P26.scala
@@ -0,0 +1,28 @@
+package konopski.ninety.nine {
+
+object P26 extends App {
+
+  def combinations[T](n: Int, list: List[T]): List[List[T]] = {
+    if(list.isEmpty || n==0 ) List()
+    else {
+      if(n==1) list map { List(_) }
+      else { 
+
+        //combinations n xs = head [ (++) (map (x:) (combinations (n-1) (filter (/=x) xs) )) (combinations n (filter (/=x) xs)) | x <- xs ]
+
+        val empty: List[List[T]] = List()
+        list.foldLeft(empty) {(ac: List[List[T]], x: T) =>
+          
+           ((combinations(n-1, list filter { _ != x})) map { 
+             l: List[T] => x::l 
+           } )++ (combinations(n, list filter { _ != x}))
+        }
+      } 
+    }
+  } 
+  
+println( combinations(2, List('a, 'b, 'c, 'd)))
+
+}
+
+}
