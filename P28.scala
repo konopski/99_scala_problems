94ea51f61e13b677465ee1237d3c81c3fa25e77b
Lukasz Konopski <lkonopski@sygnity.pl>
2016-06-10 22:58:55 +0200

    p28 b sort by size frequency

--- P28.scala
+++ P28.scala
@@ -9,13 +9,25 @@ object P28 extends App {
     }
   }
 
+  def le[T] = {(x: (List[T], Int), y: (List[T], Int)) => x._2 <= y._2 }
+
   def lsort[T](lists: List[List[T]]) = { 
-    sort(lists map { l => (l, l.size) }, {(x: (List[T], Int), y: (List[T], Int)) => x._2 <= y._2 }).unzip._1
+    sort(lists map { l => (l, l.size) }, le).unzip._1
+  }
+
+  def lsortFreq[T](lists: List[List[T]]) = { 
+    val sized = lists map { l => (l, l.size) }
+    val sizeFreq = sized map { e => (e._2, sized.count( _._2 == e._2 )) } 
+    val sf = sized map { l => (l._1, sizeFreq.find(_._1 == l._2).get._2) }
+    sort(sf, le).unzip._1
   }
 
   
 lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) foreach println 
 
+println
+
+lsortFreq(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) foreach println 
 }
 
 }
