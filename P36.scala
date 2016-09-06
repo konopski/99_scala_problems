6bf6f3b75ec3cf7b68549ea7e65de21ec055c58d
Lukasz Konopski <konopski@gmail.com>
2016-09-06 21:19:06 +0000

    P36 primefactors by multiplicity

---
+++ P36.scala
@@ -0,0 +1,13 @@
+package konopski.ninety.nine {
+
+import S99Int._
+
+object P36 extends App {
+
+
+  println(315.primeFactorsMultiplicity)
+  println(315.primeFactorsMultiplicityAsMap)
+
+}
+
+}
--- arithmetic1.scala
+++ arithmetic1.scala
@@ -2,26 +2,34 @@ package konopski.ninety.nine {
 
   class S99Int(val start: Int) {
     import S99Int._
+    import P10._
+    import P32._
 
-    def isPrime = {
+    def isPrime() = {
       (2 until this.start filter { this.start % _ == 0 }).length == 0 
     } 
 
     def isCoprimeTo(i: Int) = {
-      P32.gcd(this.start, i) == 1
+      gcd(this.start, i) == 1
     }
 
-    def primeFactors = {
+    def primeFactors(): List[Int] = {
       val primes = (2 until this.start) filter { _.isPrime }
-      def banglaj(x: Int, l: List[Int]): Seq[Int] = {
+      def banglaj(x: Int, l: List[Int]): List[Int] = {
         val nc = primes filter { _ <= x } filter { x % _ == 0 }
         if (nc.isEmpty) l
         else banglaj( x / nc(0), l :+ nc(0) )
       }
       banglaj(this.start, List())
-    }
+    } 
+    
+    def primeFactorsMultiplicity() = encode(primeFactors()) 
 
-    def totient = {
+    def primeFactorsMultiplicityAsMap() = {
+      primeFactorsMultiplicity() map { Map(_) }
+    }
+  
+    def totient() = {
         (1 to this.start filter { _.isCoprimeTo(this.start) } ).length   
     }
   }
