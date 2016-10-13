ea065f61b7f3a71e2a9040f7067c0af833984dd3
Lukasz Konopski <konopski@gmail.com>
2016-10-13 21:15:34 +0000

    fix reverse()

--- p05.scala
+++ p05.scala
@@ -1,15 +1,8 @@
 package konopski.ninety.nine { 
   object p05 {
 def reverse[A](in: List[A]) = {
-
-  def rev[A](in: List[A], out: List[A]): List[A] = {
-    if(in.isEmpty) out
-    else rev(in.tail, out :+ in.head) 
-  }
-
- 
-  if(in.isEmpty) in 
-  else rev(in.tail, List(in.head))
+  val empty: List[A] = List()
+  in.foldLeft (empty) { (acc: List[A], el: A) => el :: acc }
 }
 
 println(reverse(List(1, 1, 2, 3, 5, 8)))
