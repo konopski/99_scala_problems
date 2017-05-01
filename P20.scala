abde0e1ea717e5bcdbcde9c0649f2030bfe0fa18
Lukasz Konopski <konopski@gmail.com>
2017-05-02 00:05:35 +0200

    refactor and simplify removeAt()

--- P20.scala
+++ P20.scala
@@ -1,15 +1,13 @@
 package konopski.ninety.nine {
-import p03._
+
 object P20 extends App {
 
   def removeAt[T](n: Int, list: List[T]) = {
-    (
+
         list
         .zipWithIndex
         .filter( (p) => p._2 != n )
-        .unzip._1 ,
-        nth(n, list)
-    )
+        .unzip._1
   }
   
   println(removeAt(1, List('a, 'b, 'c, 'd)))
--- P23.scala
+++ P23.scala
@@ -1,15 +1,18 @@
 package konopski.ninety.nine {
 
+import p03._
 import P20._
 import scala.util.Random
 
 object P23 extends App {
   lazy val r = new Random
 
+  def removeAtPlus[T](n: Int, l: List[T]) = (removeAt(n, l), nth(n, l))
+
   def randomSelect[T](n: Int, list: List[T]) : List[T] = {
     if(n<=0 || list.isEmpty) List()
     else  { 
-      val afterRemove = removeAt(r.nextInt(list.size), list)
+      val afterRemove = removeAtPlus(r.nextInt(list.size), list)
       afterRemove._2 :: randomSelect(n - 1, afterRemove._1)
     }
   } 
