919c71fc39cccdaebee07c7769d0a0b794b55190
Lukasz Konopski <konopski@gmail.com>
2018-02-12 23:25:22 +0100

    prepare for trees

--- p05.scala
+++ p05.scala
@@ -1,10 +1,10 @@
 package konopski.ninety.nine { 
-  object p05 {
-def reverse[A](in: List[A]) = {
-  val empty: List[A] = List()
-  in.foldLeft (empty) { (acc: List[A], el: A) => el :: acc }
-}
+  object p05 extends App {
+	def reverse[A](in: List[A]) = {
+	  val empty: List[A] = List()
+	  in.foldLeft (empty) { (acc: List[A], el: A) => el :: acc }
+	}
 
-println(reverse(List(1, 1, 2, 3, 5, 8)))
+	println(reverse(List(1, 1, 2, 3, 5, 8)))
   }
 }
